15915d6236ff9fe85b6d2f73b22a06278e4eca1a
babsolune <babsolune@phpboost.com>
2026-05-23 19:59:55 +0200

    [UPD] lang

---
+++ api/lang/english/common.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @copyright   &copy; 2005-2026 PHPBoost
+ * @license     https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL-3.0
+ * @author      Sebastien LARTIGUE <babsolune@phpboost.com>
+ * @version     PHPBoost 6.1 - last update: 2026 05 19
+ * @since   	PHPBoost 5.1 - 2017 11 05
+ */
+
+####################################################
+#                    French                        #
+####################################################
+
+$lang['api.module.title'] = 'PHP Documentation of PHPBoost';
+$lang['api.search.placeholder'] = 'Search a class&hellip;';
+$lang['api.generation.clue'] = 'Generated from <strong>700</strong> files &middot; <strong>701</strong> classes &middot; <strong>4942</strong> methods';
